8391c204fad6961f407c4133378f520e6424a1f2
rishumour <rishumour10@gmail.com>
2026-06-29 08:03:07 +0530

    errors solved

--- JAVASCRIPT/02. BASICS/03. DATA TYPES/EXAMPLE_01.js
+++ JAVASCRIPT/02. BASICS/03. DATA TYPES/EXAMPLE_01.js
@@ -3,7 +3,7 @@ let name = "Aarav";           // String
 let isStudent = true;         // Boolean
 let value;                    // Undefined
 let empty = null;             // Null
-let big = 12345678901234567890n; // BigInt
+let big = 12345678901234567890n; // BigInt  just add "n" at the last of the number
 let id = Symbol("id");        // Symbol
 let person = { name: "Aarav" }; // Object
 
