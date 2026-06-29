88ff8e9d780dd80f6c31f31c6ad2561b784eb662
rishumour <rishumour10@gmail.com>
2026-06-29 10:24:54 +0530

    examples added

---
+++ JAVASCRIPT/02. BASICS/07. SWITCH CASE/EXAMPLE_1_Calculator.js
@@ -0,0 +1,23 @@
+let num1 = 10;
+let num2 = 5;
+let operator = "+";
+let result;
+
+switch (operator) {
+  case "+":
+    result = num1 + num2;
+    break;
+  case "-":
+    result = num1 - num2;
+    break;
+  case "*":
+    result = num1 * num2;
+    break;
+  case "/":
+    result = num1 / num2;
+    break;
+  default:
+    result = "Invalid operator";
+}
+
+console.log(result);
